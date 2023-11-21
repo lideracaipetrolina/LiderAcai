e9f770b6fa90fac3229b3f547383a3b3d361a2ef
rickshaw walis <lideracaipetrolinape@gmail.com>
2023-11-20 21:21:04 -0300

    ASD

--- Script-MudarPagina/script-MudarPagina.js
+++ Script-MudarPagina/script-MudarPagina.js
@@ -15,13 +15,13 @@ function selecionarProduto() {
 
         // Cria condições para cada ID existente
         if (produtoSelecionado.id === "copo300") {
-            window.location.href = "/copo300/ACOMPANHAMENTO/pagina-Acompanhamento.html";
+            window.location.href = "../../LiderAcai/copo300/ACOMPANHAMENTO/pagina-Acompanhamento.html";
         }
         else if (produtoSelecionado.id === "copo500") {
             window.location.href = "../../LiderAcai/copo500/ACOMPANHAMENTO/pagina-Acompanhamento.html"; // Insira o caminho da página desejada
         }
         else if (produtoSelecionado.id === "pote240") {
-            window.location.href = "../pote240/ACOMPANHAMENTO/pagina-Acompanhamento.html"; // Insira o caminho da página desejada
+            window.location.href = ""; // Insira o caminho da página desejada
         }
         else if (produtoSelecionado.id === "pote360") {
             window.location.href = ""; // Insira o caminho da página desejada
