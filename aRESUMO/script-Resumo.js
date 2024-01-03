89bc5352a1848f3575e66b7d0e09412e2cf6d6a6
lideracaipetrolina <lideracaipetrolinape@gmail.com>
2024-01-03 20:19:48 -0300

    a

--- aRESUMO/script-Resumo.js
+++ aRESUMO/script-Resumo.js
@@ -77,80 +77,26 @@ document.addEventListener('DOMContentLoaded', function () {
   <br><span style="font-weight: bold;">Ponto de Referência:</span> ${endereco.referencia}
     `;
 
-    var formasPagamento = document.getElementsByName('pagamento');
-    for (var i = 0; i < formasPagamento.length; i++) {
-      formasPagamento[i].addEventListener('change', mostrarTroco);
+  var formasPagamento = document.getElementsByName('pagamento');
+  for (var i = 0; i < formasPagamento.length; i++) {
+    formasPagamento[i].addEventListener('change', mostrarTroco);
+  }
+
+  function mostrarTroco() {
+    var escolhaPagamento = document.querySelector('input[name="pagamento"]:checked').value;
+    var trocoSection = document.getElementById('trocoSection');
+
+    if (escolhaPagamento === 'DINHEIRO') {
+      trocoSection.style.display = 'block';
+    } else {
+      trocoSection.style.display = 'none';
     }
-  
-    function mostrarTroco() {
-      var escolhaPagamento = document.querySelector('input[name="pagamento"]:checked').value;
-      var trocoSection = document.getElementById('trocoSection');
-  
-      if (escolhaPagamento === 'DINHEIRO') {
-        trocoSection.style.display = 'block';
-      } else {
-        trocoSection.style.display = 'none';
-      }
-    }
-  
-});
-//DIVISÃO DAS DUAS FUNÇÕES--------------------------------------------------
-
-
-//FORMA DE PAGAMENTO
- 
-
-//ENVIAR ZAP----------------------------------------------
-function enviarMensagemWhatsApp() {
-  // // Obtenha os dados necessários
-  const dataText = sessionStorage.getItem('dataText');
-  const valorProduto = sessionStorage.getItem('valorProduto');
-  const escolhaCobertura = JSON.parse(sessionStorage.getItem('escolhaCobertura')) || [];
-  const escolhaFrutas = JSON.parse(sessionStorage.getItem('escolhaFrutas')) || [];
-  const escolhaComplementos = JSON.parse(sessionStorage.getItem('escolhaComplementos')) || [];
-  const escolhaExtras = JSON.parse(sessionStorage.getItem('escolhaExtras')) || [];
-  const endereco = JSON.parse(sessionStorage.getItem('endereco')) || {};
-  const retiradaProduto = sessionStorage.getItem('escolhaEntrega')
-  //ACOMPANHAMENTO----------------------------------------------------------------------------------
-  function formatarObjetoParaString(objeto) {
-    return Array.isArray(objeto) ? formatarEscolhas(objeto) : JSON.stringify(objeto, null, 2);
   }
-
-  function formatarEscolha(escolha) {
-    return `${escolha.texto}: ${parseFloat(escolha.valor).toFixed(2)}`;
-  }
-
-  function formatarEscolhas(escolhas) {
-    return escolhas.map(formatarEscolha).join('\n');
-  }
-
-  // CALCULO ---------------------------------
-  const somarArray = (array) => {
-    return array.filter((item) => item && typeof item === 'object' && 'valor' in item).reduce((acumulador, item) => acumulador + parseFloat(item.valor), 0);
-  };
-
-  // Somar os valores de cada array
-  const somaCobertura = somarArray(escolhaCobertura);
-  const somaFrutas = somarArray(escolhaFrutas);
-  const somaComplementos = somarArray(escolhaComplementos);
-  const somaExtras = somarArray(escolhaExtras);
-
-  // Calcular a soma total
-  const somaTotal = parseFloat(valorProduto) + somaCobertura + somaFrutas + somaComplementos + somaExtras;
-
-  // Construir o texto com os resultados
-  document.getElementById('gastoGeral').innerHTML = `
-  <br><span style="font-weight: bold;">RESUMO TOTAL À PAGAR(R$)</span><br>
-  <span style="font-weight: bold;">Tamanho R$:</span> ${valorProduto} <br>
-  <span style="font-weight: bold;">Cobertura R$:</span> ${somaCobertura.toFixed(2)} <br>
-  <span style="font-weight: bold;">Frutas R$:</span> ${somaFrutas.toFixed(2)} <br>
-  <span style="font-weight: bold;">Complementos R$:</span> ${somaComplementos.toFixed(2)} <br>
-  <span style="font-weight: bold;">Extras R$:</span> ${somaExtras.toFixed(2)} <br>
-  <span style="font-weight: bold;">VALOR TOTAL R$:</span> ${somaTotal.toFixed(2)}`;
-
-
-  // Constrói o texto a ser enviado
-  const textoParaEnviar = `
+  //ENVIAR ZAP----------------------------------------------
+  enviarMensagemWhatsApp()
+  function enviarMensagemWhatsApp() {
+    // Constrói o texto a ser enviado
+    const textoParaEnviar = `
 *TAMANHO*
 *Produto:* ${dataText} - Valor: ${valorProduto}
 
@@ -184,8 +130,9 @@ ${retiradaProduto}
 *Ponto de Referência:* ${endereco.referencia}
 `;
 
-  const codigoPais = '55';
-  const numeroTelefone = '8791793828';
-  const linkWhatsApp = `https://wa.me/${codigoPais}${numeroTelefone}?text=${encodeURIComponent(textoParaEnviar)}`;
-  window.open(linkWhatsApp, '_blank');
-}
+    const codigoPais = '55';
+    const numeroTelefone = '8791793828';
+    const linkWhatsApp = `https://wa.me/${codigoPais}${numeroTelefone}?text=${encodeURIComponent(textoParaEnviar)}`;
+    window.open(linkWhatsApp, '_blank');
+  }
+});
