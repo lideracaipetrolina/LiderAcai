74c7475cae9c48f1a5279bd986dba20e60879ef9
lideracaipetrolina <lideracaipetrolinape@gmail.com>
2024-02-05 02:11:26 -0300

    c

--- formapagamento/script.pagamento.js
+++ formapagamento/script.pagamento.js
@@ -1,131 +1,118 @@
 //FORMA DE PAGAMENTO
 
 const FormaPagamento = () => {
-    let formasPagamento = document.getElementsByName('pagamento');
-    for (let i = 0; i < formasPagamento.length; i++) {
-        formasPagamento[i].addEventListener('change', mostrarTroco);
+  let formasPagamento = document.getElementsByName('pagamento');
+  for (let i = 0; i < formasPagamento.length; i++) {
+    formasPagamento[i].addEventListener('change', mostrarTroco);
+  }
+
+  function mostrarTroco() {
+    let escolhaPagamento = document.querySelector('input[name="pagamento"]:checked').value;
+
+
+    let trocoSection = document.getElementById('trocoSection');
+
+    if (escolhaPagamento) {
+      if (escolhaPagamento === 'DINHEIRO') {
+        trocoSection.style.display = 'block';
+        sessionStorage.setItem('formaPagamento', 'DINHEIRO');
+      }
+      else if (escolhaPagamento === 'CARTÃO' || escolhaPagamento === 'PIX') {
+        trocoSection.style.display = 'none';
+        sessionStorage.setItem('formaPagamento', escolhaPagamento);
+      }
+    }
+  }
+
+
+  const Troco = () => {
+    let valorTroco = document.getElementById('valorTroco').value.trim();
+    if (valorTroco) {
+      sessionStorage.setItem('Vtroco', valorTroco);
     }
 
-    function mostrarTroco() {
-        let escolhaPagamento = document.querySelector('input[name="pagamento"]:checked').value;
+  }
 
+  const VerificarDados = () => {
+    // Verifica se pelo menos uma forma de pagamento foi escolhida antes de prosseguir
 
-        let trocoSection = document.getElementById('trocoSection');
-
-        if (escolhaPagamento) {
-            if (escolhaPagamento === 'DINHEIRO') {
-                trocoSection.style.display = 'block';
-                sessionStorage.setItem('formaPagamento', 'DINHEIRO');
-            }
-            else if (escolhaPagamento === 'CARTÃO' || escolhaPagamento === 'PIX') {
-                trocoSection.style.display = 'none';
-                sessionStorage.setItem('formaPagamento', escolhaPagamento);
-            }
-        }
+    if (sessionStorage.getItem('formaPagamento')) {
+      enviarMensagemWhatsApp();
     }
-
-
-    const Troco = () => {
-        let valorTroco = document.getElementById('valorTroco').value.trim();
-        if (valorTroco) {
-            sessionStorage.setItem('Vtroco', valorTroco);
-        }
-
+    else if (!sessionStorage.getItem('formaPagamento')) {
+      alert("Escolha uma forma de pagamento!");
+      return true; // Impede a continuação se nenhuma forma de pagamento foi escolhida
     }
+  }
 
-    const VerificarDados = () => {
-        // Verifica se pelo menos uma forma de pagamento foi escolhida antes de prosseguir
-        
-        if(sessionStorage.getItem('formaPagamento')) {
-            enviarMensagemWhatsApp();
-        }
-       else if (!sessionStorage.getItem('formaPagamento')) {
-            alert("Escolha uma forma de pagamento!");
-            return true; // Impede a continuação se nenhuma forma de pagamento foi escolhida
-        } 
-    }
-
-    document.querySelector("#Fpagamento").addEventListener("click", () => {
-        Troco()
-        VerificarDados()
-        FormaPagamento()
-    });
+  document.querySelector("#Fpagamento").addEventListener("click", () => {
+    Troco()
+    VerificarDados()
+    FormaPagamento()
+  });
 };
 
 document.addEventListener('DOMContentLoaded', FormaPagamento);
 
 
-const enviarMensagemWhatsApp=()=> {
-    let somaGeral = 0
-    let pedidoGeral = 0
-    let textoParaEnviar = '';
-  
-  
-    const pedidos = (pedido)=> {
-      pedidoGeral+= pedido
-    };
-
-    const calcular = (somaTotal)=> {
-      somaGeral += somaTotal;
-    };
+const enviarMensagemWhatsApp = () => {
+  let somaGeral = 0
+  let textoParaEnviar = '';
 
 
-    
-    for (let i = 0; i < sessionStorage.length; i++) {
-      const chaveProduto = `escolhaProduto_${i}`;
-      const chaveValor = `escolhaProdutoValor_${i}`;
+  const gerarNumeroAleatorio = () => {
+    return Math.floor(Math.random() * 1000) + 1;
+  };
+
+  const criarPedido = () => {
+    const numeroPedido = gerarNumeroAleatorio();
+    return numeroPedido;
+  };
+
+  const numeroPedido = criarPedido();
   
-      const chaveCobertura = `escolhaCobertura_${i}`;
-      const chaveFruta = `escolhaFruta_${i}`;
-      const chaveComplemento = `escolhaComplemento_${i}`;
-      const chaveExtra = `escolhaExtras_${i}`;
-  
-      const escolhaProduto = sessionStorage.getItem(chaveProduto);
-      const escolhaValor = parseFloat(sessionStorage.getItem(chaveValor));
-      const escolhaCobertura = JSON.parse(sessionStorage.getItem(chaveCobertura)) || [];
-      const escolhaFrutas = JSON.parse(sessionStorage.getItem(chaveFruta)) || [];
-      const escolhaComplementos = JSON.parse(sessionStorage.getItem(chaveComplemento)) || [];
-      const escolhaExtras = JSON.parse(sessionStorage.getItem(chaveExtra)) || [];
-  
-  
-      if (escolhaProduto && !isNaN(escolhaValor)) {
-        let div = document.createElement('div');
-        div.setAttribute("class", "mercadoria");
-  
-        // Exibir ACOMPANHAMENTOS---------------------------------------------------
-        function formatarObjetoParaString(objeto) {
-          return Array.isArray(objeto) ? formatarEscolhas(objeto) : JSON.stringify(objeto, null, 2);
-        }
-  
-        function formatarEscolha(escolha) {
-          return `${escolha.texto}: ${parseFloat(escolha.valor).toFixed(2)}`;
-        }
-  
+  const calcular = (somaTotal) => {
+    somaGeral += somaTotal;
+  };
+
+
+
+  for (let i = 0; i < sessionStorage.length; i++) {
+    const chaveProduto = `escolhaProduto_${i}`;
+    const chaveValor = `escolhaProdutoValor_${i}`;
+
+    const chaveCobertura = `escolhaCobertura_${i}`;
+    const chaveFruta = `escolhaFruta_${i}`;
+    const chaveComplemento = `escolhaComplemento_${i}`;
+    const chaveExtra = `escolhaExtras_${i}`;
+
+    const escolhaProduto = sessionStorage.getItem(chaveProduto);
+    const escolhaValor = parseFloat(sessionStorage.getItem(chaveValor));
+    const escolhaCobertura = JSON.parse(sessionStorage.getItem(chaveCobertura)) || [];
+    const escolhaFrutas = JSON.parse(sessionStorage.getItem(chaveFruta)) || [];
+    const escolhaComplementos = JSON.parse(sessionStorage.getItem(chaveComplemento)) || [];
+    const escolhaExtras = JSON.parse(sessionStorage.getItem(chaveExtra)) || [];
+
+
+    if (escolhaProduto && !isNaN(escolhaValor)) {
+      let div = document.createElement('div');
+      div.setAttribute("class", "mercadoria");
+
+      // Exibir ACOMPANHAMENTOS---------------------------------------------------
+      function formatarObjetoParaString(objeto) {
+        return Array.isArray(objeto) ? formatarEscolhas(objeto) : JSON.stringify(objeto, null, 2);
+      }
+
+      function formatarEscolha(escolha) {
+        return `${escolha.texto}: ${parseFloat(escolha.valor).toFixed(2)}`;
+      }
+
       function formatarEscolhas(escolhas) {
-    return escolhas.map(formatarEscolha).join('\n');
-  }
-
-// Função para gerar um número aleatório entre 1 e 1000
-function gerarNumeroAleatorio() {
-  return Math.floor(Math.random() * 1000) + 1;
-}
-
-// Função para criar um pedido com um número aleatório
-function criarPedido() {
-  const numeroPedido = gerarNumeroAleatorio();
-  return `Pedido nº ${numeroPedido}`;
-}
-
-// Exemplo de uso
-for (let i = 1; i <= 5; i++) {
-  const pedido = criarPedido();
-  pedidos(pedido)
-}
-
-
-
-        textoParaEnviar += `
-        *PEDIDO Nª ${pedidoGeral}
+        return escolhas.map(formatarEscolha).join('\n');
+      }
+     
+      textoParaEnviar += `
+        *PEDIDO Nª ${numeroPedido}
         *PEDIDO LIDER AÇAÍ*
         \n*PRODUTO:* ${escolhaProduto} - R$ ${escolhaValor.toFixed(2)}
         \n*ACOMPANHAMENTOS*
@@ -135,22 +122,22 @@ for (let i = 1; i <= 5; i++) {
         \n*EXTRAS:*  \n${formatarObjetoParaString(escolhaExtras)}
   
    `;
-        // CALCULO ---------------------------------
-        const somarArray = (array) => {
-          return array.filter((item) => item && typeof item === 'object' && 'valor' in item).reduce((acumulador, item) => acumulador + parseFloat(item.valor), 0);
-        };
-  
-        // Somar os valores de cada array
-        const somaCobertura = somarArray(escolhaCobertura);
-        const somaFrutas = somarArray(escolhaFrutas);
-        const somaComplementos = somarArray(escolhaComplementos);
-        const somaExtras = somarArray(escolhaExtras);
-  
-        // Calcular a soma total
-        const somaTotal = parseFloat(escolhaValor) + somaCobertura + somaFrutas + somaComplementos + somaExtras;
-  
-        // Construir o texto com os resultados
-        textoParaEnviar += `
+      // CALCULO ---------------------------------
+      const somarArray = (array) => {
+        return array.filter((item) => item && typeof item === 'object' && 'valor' in item).reduce((acumulador, item) => acumulador + parseFloat(item.valor), 0);
+      };
+
+      // Somar os valores de cada array
+      const somaCobertura = somarArray(escolhaCobertura);
+      const somaFrutas = somarArray(escolhaFrutas);
+      const somaComplementos = somarArray(escolhaComplementos);
+      const somaExtras = somarArray(escolhaExtras);
+
+      // Calcular a soma total
+      const somaTotal = parseFloat(escolhaValor) + somaCobertura + somaFrutas + somaComplementos + somaExtras;
+
+      // Construir o texto com os resultados
+      textoParaEnviar += `
         ----------------------------------------
         \n*RESUMO TOTAL À PAGAR(R$)*
         *TAMANHO R$:* ${escolhaValor.toFixed(2)}
@@ -160,26 +147,26 @@ for (let i = 1; i <= 5; i++) {
         *EXTRAS R$:* ${somaExtras.toFixed(2)}
         *VALOR TOTAL R$:* ${somaTotal.toFixed(2)}
   `;
-  
-        calcular(somaTotal)
-      }
+
+      calcular(somaTotal)
     }
-  
-    
-  
-  
-    const endereco = JSON.parse(sessionStorage.getItem('endereco')) || {};
-    const retiradaProduto = sessionStorage.getItem('escolhaEntrega')
-  
-    const formaPagamento = sessionStorage.getItem('formaPagamento');
-    const valorTroco = sessionStorage.getItem('Vtroco');
-  
-    // Verifica se o endereço foi preenchido
-    const enderecoPreenchido = (endereco.nomeRua || endereco.numeroCasa || endereco.cep || endereco.cidade || endereco.bairro || endereco.referencia);
-  
-    let enderecoTexto = '';
-    if (enderecoPreenchido) {
-      enderecoTexto = `
+  }
+
+
+
+
+  const endereco = JSON.parse(sessionStorage.getItem('endereco')) || {};
+  const retiradaProduto = sessionStorage.getItem('escolhaEntrega')
+
+  const formaPagamento = sessionStorage.getItem('formaPagamento');
+  const valorTroco = sessionStorage.getItem('Vtroco');
+
+  // Verifica se o endereço foi preenchido
+  const enderecoPreenchido = (endereco.nomeRua || endereco.numeroCasa || endereco.cep || endereco.cidade || endereco.bairro || endereco.referencia);
+
+  let enderecoTexto = '';
+  if (enderecoPreenchido) {
+    enderecoTexto = `
                  \n*ENDEREÇO PARA ENTREGA*
                  *Nome da Rua:* ${endereco.nomeRua || 'Não fornecido'}
                  *Número da Casa/AP:* ${endereco.numeroCasa || 'Não fornecido'}
@@ -188,12 +175,12 @@ for (let i = 1; i <= 5; i++) {
                  *Bairro:* ${endereco.bairro || 'Não fornecido'}
                  *Ponto de Referência:* ${endereco.referencia || 'Não fornecido'}
              `;
-    }
-  
+  }
+
   textoParaEnviar += `
   \n*VALOR GERAL:*  R$ ${somaGeral.toFixed(2)}`
 
-  
+
   if (formaPagamento) {
     textoParaEnviar += `
     ----------------------------------------
@@ -206,17 +193,17 @@ for (let i = 1; i <= 5; i++) {
     *TROCO:* ${valorTroco}
     `;
   }
-  
-    textoParaEnviar += ` 
+
+  textoParaEnviar += ` 
     ----------------------------------------
     \n\n*RETIRADA NO LOCAL*: ${retiradaProduto}`
-  
-    textoParaEnviar += `${enderecoTexto}`
-  
-  
-    const codigoPais = '55';
-    const numeroTelefone = '87991614277';
-  
-    const linkWhatsApp = `https://wa.me/${codigoPais}${numeroTelefone}?text=${encodeURIComponent(textoParaEnviar)}`;
-    window.open(linkWhatsApp, '_blank');
-  }
+
+  textoParaEnviar += `${enderecoTexto}`
+
+
+  const codigoPais = '55';
+  const numeroTelefone = '87991614277';
+
+  const linkWhatsApp = `https://wa.me/${codigoPais}${numeroTelefone}?text=${encodeURIComponent(textoParaEnviar)}`;
+  window.open(linkWhatsApp, '_blank');
+}
