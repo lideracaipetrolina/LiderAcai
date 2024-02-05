04ac3e1d81823ac785725d6a00d90aac698fe073
lideracaipetrolina <lideracaipetrolinape@gmail.com>
2024-02-04 21:44:30 -0300

    d

--- formapagamento/script.pagamento.js
+++ formapagamento/script.pagamento.js
@@ -1,6 +1,8 @@
 //FORMA DE PAGAMENTO
 
-import { enviarZap } from '../EnviarZap/scriptEnviarZap';
+import { enviarMensagemWhatsApp } from '../EnviarZap/scriptEnviarZap';
+
+
 const FormaPagamento = () => {
     let formasPagamento = document.getElementsByName('pagamento');
     for (let i = 0; i < formasPagamento.length; i++) {
@@ -38,7 +40,7 @@ const FormaPagamento = () => {
         // Verifica se pelo menos uma forma de pagamento foi escolhida antes de prosseguir
         
         if(sessionStorage.getItem('formaPagamento')) {
-            enviarZap();
+            enviarMensagemWhatsApp();
         }
        else if (!sessionStorage.getItem('formaPagamento')) {
             alert("Escolha uma forma de pagamento!");
