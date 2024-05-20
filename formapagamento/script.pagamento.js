e485684b68994a640850141ba80dba405867e855
lideracaipetrolina <lideracaipetrolinape@gmail.com>
2024-05-20 13:15:09 -0300

    dasd

--- formapagamento/script.pagamento.js
+++ formapagamento/script.pagamento.js
@@ -124,7 +124,7 @@ const enviarMensagemWhatsApp=()=> {
         \n*PEDIDO Nº:* ${numeroPedido}
         *PRODUTO:* ${escolhaProduto} - R$ ${escolhaValor.toFixed(2)}
         *QUANTIDADE:* ${escolhaQuantidade}
-        \n*CREME:* ${formatarObjetoParaString(escolhaCreme)}
+        \n*CREME:* \n ${formatarObjetoParaString(escolhaCreme)}
         \n*ACOMPANHAMENTOS*
         *COBERTURA:* \n ${formatarObjetoParaString(escolhaCobertura)}
         *FRUTAS:*  \n${formatarObjetoParaString(escolhaFrutas)}
